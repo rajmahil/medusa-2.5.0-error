aeb545d06070f89243b21a29566ab1047b6a6d76
Raj Mahil <webtest10111011@gmail.com>
2025-02-11 18:27:26 -0600

    update

---
+++ src/api/admin/custom/route.ts
@@ -0,0 +1,7 @@
+import type { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
+import { batchShippingOptionRulesWorkflow } from "@medusajs/medusa/core-flows";
+
+export async function POST(req: MedusaRequest, res: MedusaResponse) {
+
+  res.send();
+}
